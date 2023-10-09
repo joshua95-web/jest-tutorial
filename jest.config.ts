32ac7b1b4a6c7b380b20b156c26e9974e6f6f5bb
Joshua <122205306+joshua95-web@users.noreply.github.com>
2023-10-09 10:51:54 +0100

    added typescript configuration to babel config file

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,9 @@ import type { Config } from "jest";
 export default async (): Promise<Config> => {
   return {
     verbose: true,
+    transform: {
+      "^.+\\.(ts|tsx|js|jsx)$": "ts-jest",
+    },
   };
 };
 
